ae719ba5cd841bf11a5fd5464c2017ba978c5a8d
einsfr <einsfr@users.noreply.github.com>
2016-12-12 09:41:30 +0300

    rest framework 3.5 deprecation fix

--- cmc/conf/common.py
+++ cmc/conf/common.py
@@ -63,7 +63,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend', ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend', ),
     'PAGE_SIZE': 20,
 }
 
