085ad579cec23984380d81147a09afd3171a2839
einsfr <einsfr@users.noreply.github.com>
2016-12-14 14:41:25 +0300

    celery excluded =)

--- cmc/celery.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_NODULE', 'cmc.settings')
-
-app = Celery('cmc')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
